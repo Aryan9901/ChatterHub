45d95478653393d46e7b8006e84de50c649b61d6
Aryan9901 <911aaryan@gmail.com>
2024-12-21 21:26:05 +0530

    userController crud api added

--- server/src/main/java/com/aryan/app/ChatterHubApplication.java
+++ server/src/main/java/com/aryan/app/ChatterHubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatterHubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChatterHubApplication.class, args);
 	}
--- server/src/main/java/com/aryan/app/controller/UserController.java
+++ server/src/main/java/com/aryan/app/controller/UserController.java
@@ -4,7 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.aryan.app.models.User;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserController {
@@ -13,9 +19,9 @@ public class UserController {
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
 
-        User user1 = new User("Aryan", "Gupta", "911aaryan@gmail.com", "password");
-        User user2 = new User("Zosh", "Code", "zosh.code@gmail.com", "codezosh");
-        User user3 = new User("Ayush", "kumar", "ayush10@gmail.com", "ayush@123");
+        User user1 = new User(1, "Aryan", "Gupta", "911aaryan@gmail.com", "password");
+        User user2 = new User(2, "Zosh", "Code", "zosh.code@gmail.com", "codezosh");
+        User user3 = new User(3, "Ayush", "kumar", "ayush10@gmail.com", "ayush@123");
         users.add(user1);
         users.add(user2);
         users.add(user3);
@@ -23,4 +29,34 @@ public class UserController {
         return users;
     }
 
+    @GetMapping("/users/{userid}")
+    public User getUserById(@PathVariable("userid") int id) {
+        System.out.println(id);
+        User user = new User(1, "Aryan", "Gupta", "911aaryan@gmail.com", "password");
+        user.setId(id);
+        return user;
+    }
+
+    @PostMapping("/users/create")
+    public User createUser(@RequestBody User user) {
+        System.out.println(user);
+        return user;
+    }
+
+    @PutMapping("/users/update")
+    public User updateUser(@RequestBody User user) {
+        User updatedUser = new User();
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setId(user.getId());
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setPassword(user.getPassword());
+
+        return updatedUser;
+    }
+
+    @DeleteMapping("/users/{userid}")
+    public String deleteUser(@PathVariable int userid) {
+        return "user deleted successfully with id " + userid;
+    }
 }

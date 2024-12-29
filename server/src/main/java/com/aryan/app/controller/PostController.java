639592cd166b1236623fc1bcd7499fdfdc25c474
Aryan9901 <911aaryan@gmail.com>
2024-12-29 12:34:59 +0530

    added a base route /api/ for every post endpoints

--- server/src/main/java/com/aryan/app/controller/PostController.java
+++ server/src/main/java/com/aryan/app/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aryan.app.models.entity.Post;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@RequestMapping("/api")
 @AllArgsConstructor
 public class PostController {
     PostService postService;

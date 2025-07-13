d70449b680291adb2797cafc538b8884eb39a5de
ARYAN GUPTA <911aaryan@gmail.com>
2025-07-13 13:19:48 +0530

    Update User.java

--- server/src/main/java/com/aryan/app/models/entity/User.java
+++ server/src/main/java/com/aryan/app/models/entity/User.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+
+
+
+
+
+
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,9 +19,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
 
 @Data
 @NoArgsConstructor
